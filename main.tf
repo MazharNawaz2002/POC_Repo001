0228f9ce15ef422f2a08d7e8bdd4c1f76289e6c5
MazharNawaz2002 <nawazmazhar60@gmail.com>
2024-10-09 23:22:50 +0500

    SAM PRATICE 17.0

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "test_lambda1" {
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "python3.12"
+  runtime = "python3.10"
 
   environment {
     variables = {
